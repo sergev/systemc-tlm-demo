248075e61f404f45a1dc86d83ad25d87de5c51c7
Serge Vakulenko <serge.vakulenko@gmail.com>
2021-01-26 16:52:05 -0800

    Add one more test: check time.

---
+++ test_run3.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include <systemc.h>
+
+TEST(tlm, run3)
+{
+    // Set the clock frequency to 1 GHz.
+    sc_core::sc_time const clock_period(1, sc_core::SC_NS);
+
+    // Wait for SystemC initialization.
+    sc_core::sc_start(sc_core::SC_ZERO_TIME);
+
+    // Step the model forward.
+    std::cout << sc_core::sc_time_stamp() << ": step" << std::endl;
+    sc_core::sc_start(clock_period * 1);
+
+    std::cout << sc_core::sc_time_stamp() << ": step" << std::endl;
+    sc_core::sc_start(clock_period * 1);
+
+    std::cout << sc_core::sc_time_stamp() << ": step" << std::endl;
+    sc_core::sc_start(clock_period * 1);
+
+    // Stop the SystemC simulation.
+    sc_core::sc_stop();
+
+    EXPECT_EQ(sc_core::sc_time_stamp(), clock_period * 3);
+}
